201e03b168a9f1b902ddf8a8f91ffb6e5fa04529
Manish Patel <95333854+ermanish081190@users.noreply.github.com>
2026-04-07 23:41:54 +0530

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -1,19 +1,22 @@
-import { Outlet, Meta, Links, Scripts } from "react-router";
-import { addDocumentResponseHeaders } from "./shopify.server";
-
-export const headers = (headersArgs) => {
-  return addDocumentResponseHeaders(headersArgs);
-};
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
 export default function App() {
   return (
-    <html>
+    <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
         <Meta />
         <Links />
       </head>
       <body>
         <Outlet />
+        <ScrollRestoration />
         <Scripts />
       </body>
     </html>
