71d0ff04e0b47308829bcfe2d57e16800c379a9e
Manish Patel <95333854+ermanish081190@users.noreply.github.com>
2026-04-07 11:58:21 +0530

    Update app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -19,10 +19,10 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey} host={host}>
-      <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
-      </s-app-nav>
+      <nav>
+        <a href="/app">Home</a> |{" "}
+        <a href="/app/additional">Additional page</a>
+      </nav>
       <Outlet />
     </AppProvider>
   );
