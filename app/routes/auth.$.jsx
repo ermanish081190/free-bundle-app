07b43a20cc94c2c5ca5d2170dded15682984374b
Manish Patel <95333854+ermanish081190@users.noreply.github.com>
2026-04-07 14:03:11 +0530

    Update auth.$.jsx

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -2,20 +2,11 @@ import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  const { admin } = await authenticate.admin(request, {
-    returnHeaders: true,
-  });
+  await authenticate.admin(request);
 
   return null;
 };
 
 export const headers = (headersArgs) => {
-  const headers = boundary.headers(headersArgs);
-
-  return {
-    ...headers,
-    ...headersArgs.loaderHeaders, // 👈 REQUIRED for auth redirect
-    "Content-Security-Policy":
-      "frame-ancestors https://admin.shopify.com https://admin.shopify.com/store/trvlbuddy;",
-  };
+  return boundary.headers(headersArgs);
 };
