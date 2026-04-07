e2f1b22de6f3241b4d75ac8a82cb7c4780f2ed26
Manish Patel <95333854+ermanish081190@users.noreply.github.com>
2026-04-08 01:22:48 +0530

    Update app._index.jsx

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -33,7 +33,7 @@ export const action = async ({ request }) => {
 
   //const FUNCTION_ID = "019d6107-3295-7ae3-b398-bd3ca1bdd631";
   const FUNCTION_ID = "019d6958-5a3a-7ab0-8d6f-0dec55c2497a";
-  const DISCOUNT_TITLE = "Bundle Discount";
+  const DISCOUNT_TITLE = "BUNDLE DISCOUNT";
 
   const mutation = `
     mutation {
